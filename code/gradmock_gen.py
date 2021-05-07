76c3902943c3f1c2d1d6fe1923e2bdf9ca82a17c
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-06 22:19:19 -0400

    Update gradmock_gen.py

--- code/gradmock_gen.py
+++ code/gradmock_gen.py
@@ -24,7 +24,7 @@ def generate_gradmock(grad_dim, m, b, path_to_lognorm_source, lognorm_file, path
     assert isinstance(z_max, (int, float))
 
     # load in lognormal set
-    Lx, Ly, Lz, N, data = read_lognormal.read(os.path.join(path_to_lognorm_source, lognorm_file))
+    Lx, Ly, Lz, N, data = read_lognormal.read(os.path.join(path_to_lognorm_source, f"{lognorm_file}.bin"))
         # define boxsize based on mock; and N = number of data points
     L = Lx
         # L = boxsize

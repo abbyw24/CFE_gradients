2e9b0933cecc8ac16e83153a9fd9b6c533409991
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-07 01:21:42 -0400

    Update run_gradmock_gen.py

--- code/run_gradmock_gen.py
+++ code/run_gradmock_gen.py
@@ -15,13 +15,17 @@ path_to_lognorm_source = "/scratch/ksf293/mocks/lognormal/cat_L750_n2e-4_z057_pa
 lognorm_file = "cat_L750_n2e-4_z057_patchy_lognormal_rlz0" #.bin
 path_to_mocks_dir = f"mocks/{grad_dim}D/{lognorm_file}"
 
-# loop through m and b arrays
-for m in m_arr_perL:
-    for b in b_arr:
-        mock_name = "m-{:.2f}-L_b-{:.2f}".format(m, b)
-        generate_gradmock(grad_dim, m, b, path_to_lognorm_source, lognorm_file, path_to_mocks_dir, mock_name)
+# # loop through m and b arrays
+# for m in m_arr_perL:
+#     for b in b_arr:
+#         mock_name = "m-{:.2f}-L_b-{:.2f}".format(m, b)
+#         generate_gradmock(grad_dim, m, b, path_to_lognorm_source, lognorm_file, path_to_mocks_dir, mock_name)
 
-# # loop through lognormal realizations with m=0, b=0.5
-# for n in range(0,101):
-#     lognorm_file = f"cat_L750_n2e-4_z057_patchy_lognormal_rlz{n}" #.bin
-#     mock_name = m-0
+# loop through lognormal realizations with m=0, b=0.5
+for n in range(0,101):
+    lognorm_file = f"cat_L750_n2e-4_z057_patchy_lognormal_rlz{n}" #.bin
+    m = 0.00
+    b = 0.50
+    mock_name = "m-{:.2f}-L_b-{:.2f}".format(m, b)
+
+    generate_gradmock(grad_dim, m, b, path_to_lognorm_source, lognorm_file, path_to_mocks_dir, mock_name)

c88e7828566871e8ad5dd84c8a459453ebd8ad21
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-17 17:52:50 -0400

    Update exp_vs_rec_scatter.py

--- code/exp_vs_rec_scatter.py
+++ code/exp_vs_rec_scatter.py
@@ -76,13 +76,13 @@ grads_rec_s = []
 for m in m_arr_perL:
     for b in b_arr:
         mock_name = "m-{:.2f}-L_b-{:.2f}".format(m, b)
-        patches_data = np.load(f"patches/lst_sq_fit/exp_vs_rec_vals/patches_exp_vs_rec_{n_patches}patches_{mock_name}.npy", allow_pickle=True).item()
+        patches_data = np.load(os.path.join(path_to_mocks_dir, f"patches/lst_sq_fit/exp_vs_rec_vals/patches_exp_vs_rec_{n_patches}patches_{mock_name}.npy"), allow_pickle=True).item()
         grad_exp_p = patches_data["grad_expected"]
         grad_rec_p = patches_data["grad_recovered"]
         grads_exp_p.append(grad_exp_p)
         grads_rec_p.append(grad_rec_p)
         
-        suave_data = np.load(f"suave/recovered/exp_vs_rec_vals/suave_exp_vs_rec_{mock_name}.npy", allow_pickle=True).item()
+        suave_data = np.load(os.path.join(path_to_mocks_dir, f"suave/recovered/exp_vs_rec_vals/suave_exp_vs_rec_{mock_name}.npy"), allow_pickle=True).item()
         grad_exp_s = suave_data["grad_expected"]
         grad_rec_s = suave_data["grad_recovered"]
         grads_exp_s.append(grad_exp_s)

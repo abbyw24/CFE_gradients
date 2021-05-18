09142913d0933495f4be1063a331d703c9ebcbe9
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-18 04:47:43 -0400

    fixing scatter plot
    adjusting for multiple realizations

--- code/exp_vs_rec_scatter.py
+++ code/exp_vs_rec_scatter.py
@@ -4,10 +4,13 @@ import os
 
 import globals
 from create_subdirs import create_subdirs
+from extract_grads import extract_grads_exp_vs_rec
 
 globals.initialize_vals()  # brings in all the default parameters
 
 grad_dim = globals.grad_dim
+
+lognorm_file_arr = globals.lognorm_file_arr
 
 m_arr_perL = globals.m_arr_perL
 b_arr = globals.b_arr
@@ -26,7 +29,7 @@ def label_p(k):
     else:
         return None
 
-def scatter_exp_vs_rec(grads_exp_patches, grads_rec_patches, grads_exp_suave, grads_rec_suave, path_to_mocks_dir):
+def scatter_exp_vs_rec(type, grads_exp_patches, grads_rec_patches, grads_exp_suave, grads_rec_suave, path_to_scatter_dir):
     dim = {
             0 : "x",
             1 : "y",
@@ -34,16 +37,10 @@ def scatter_exp_vs_rec(grads_exp_patches, grads_rec_patches, grads_exp_suave, gr
             }
     k = 0
 
-    # create desired path to mocks directory if it doesn't already exist
-    sub_dirs = ["plots/scatter"]
-    create_subdirs(path_to_mocks_dir, sub_dirs)
-    path_to_scatter_dir = os.path.join(path_to_mocks_dir, f"plots/scatter")
-    scatter_name = f"scatter_patches_vs_suave"
-
     # make sure all inputs have the right form
     assert grads_exp_patches.shape == grads_exp_suave.shape == grads_rec_patches.shape == grads_rec_suave.shape
     # grads.shape == (201, 3)
-    assert isinstance(path_to_mocks_dir, str)
+    assert isinstance(path_to_scatter_dir, str)
 
     for i in dim:
         # create plot
@@ -61,33 +58,17 @@ def scatter_exp_vs_rec(grads_exp_patches, grads_rec_patches, grads_exp_suave, gr
         x = np.linspace(min(grads_exp_patches[:,i]), max(grads_exp_patches[:,i]), 10)
         plt.plot(x, x, color="black", alpha=0.5)
         plt.legend()
+
+        if type == "1rlz":
+            sub_dirs = ["plots/scatter"]
+            create_subdirs(path_to_mocks_dir, sub_dirs)
+            path_to_scatter_dir = f"mocks/{grad_dim}D/plots/scatter")
+        elif type == "1m":
+            sub_dirs = ["plots/scatter"]
+            create_subdirs(f"mocks/{grad_dim}D", sub_dirs)
+            path_to_scatter_dir = os.path.join()
+        else:
+            print("'type' must be '1rlz' or '1m'")
         
-        fig.savefig(os.path.join(path_to_scatter_dir, f"{scatter_name}_{dim[i]}.png"))
+        fig.savefig(os.path.join(path_to_scatter_dir, f"scatter_patches_vs_suave_{dim[i]}.png"))
         plt.cla()
-
-def scatter_patches_vs_suave_1rlz(mock_name, path_to_mocks_dir):
-    grads_exp_p = []
-    grads_rec_p = []
-    grads_exp_s = []
-    grads_rec_s = []
-    # loop through m and b values
-    for m in m_arr_perL:
-        for b in b_arr:
-            # mock_name = "m-{:.2f}-L_b-{:.2f}".format(m, b)
-            patches_data = np.load(os.path.join(path_to_mocks_dir, f"patches/lst_sq_fit/exp_vs_rec_vals/patches_exp_vs_rec_{n_patches}patches_{mock_name}.npy"), allow_pickle=True).item()
-            grad_exp_p = patches_data["grad_expected"]
-            grad_rec_p = patches_data["grad_recovered"]
-            grads_exp_p.append(grad_exp_p)
-            grads_rec_p.append(grad_rec_p)
-            
-            suave_data = np.load(os.path.join(path_to_mocks_dir, f"suave/recovered/exp_vs_rec_vals/suave_exp_vs_rec_{mock_name}.npy"), allow_pickle=True).item()
-            grad_exp_s = suave_data["grad_expected"]
-            grad_rec_s = suave_data["grad_recovered"]
-            grads_exp_s.append(grad_exp_s)
-            grads_rec_s.append(grad_rec_s)
-    grads_exp_p = np.array(grads_exp_p)
-    grads_rec_p = np.array(grads_rec_p)
-    grads_exp_s = np.array(grads_exp_s)
-    grads_rec_s = np.array(grads_rec_s)
-
-    scatter_exp_vs_rec(grads_exp_p, grads_rec_p, grads_exp_s, grads_rec_s, path_to_mocks_dir)

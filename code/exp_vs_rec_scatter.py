cd632c6fba56f8c14cecda429465ae2eb9b6afdd
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-18 05:09:06 -0400

    fixing multiple realizations

--- code/exp_vs_rec_scatter.py
+++ code/exp_vs_rec_scatter.py
@@ -31,7 +31,7 @@ def label_p(k):
     else:
         return None
 
-def scatter_exp_vs_rec(grads_exp_patches, grads_rec_patches, grads_exp_suave, grads_rec_suave, path_to_mocks_dir):
+def scatter_exp_vs_rec(grads_exp_patches, grads_rec_patches, grads_exp_suave, grads_rec_suave, path_to_scatter_dir):
     dim = {
             0 : "x",
             1 : "y",
@@ -61,16 +61,16 @@ def scatter_exp_vs_rec(grads_exp_patches, grads_rec_patches, grads_exp_suave, gr
         plt.plot(x, x, color="black", alpha=0.5)
         plt.legend()
 
-        if grad_type == "1rlz":
-            sub_dirs = ["plots/scatter"]
-            create_subdirs(path_to_mocks_dir, sub_dirs)
-            path_to_scatter_dir = f"mocks/{grad_dim}D/plots/scatter")
-        elif grad_type == "1m":
-            sub_dirs = ["plots/scatter"]
-            create_subdirs(f"mocks/{grad_dim}D", sub_dirs)
-            path_to_scatter_dir = os.path.join()
-        else:
-            print("'grad_type' must be '1rlz' or '1m'")
+        # if grad_type == "1rlz":
+        #     sub_dirs = ["plots/scatter"]
+        #     create_subdirs(path_to_mocks_dir, sub_dirs)
+        #     path_to_scatter_dir = f"mocks/{grad_dim}D/plots/scatter")
+        # elif grad_type == "1m":
+        #     sub_dirs = ["plots/scatter"]
+        #     create_subdirs(f"mocks/{grad_dim}D", sub_dirs)
+        #     path_to_scatter_dir = os.path.join()
+        # else:
+        #     print("'grad_type' must be '1rlz' or '1m'")
         
         fig.savefig(os.path.join(path_to_scatter_dir, f"scatter_patches_vs_suave_{dim[i]}.png"))
         plt.cla()

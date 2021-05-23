7bd8e5be68720bd58ce6e1dd681e3b876139c94e
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-23 15:21:48 -0400

    Update new_gradmock_gen.py

--- code/new_gradmock_gen.py
+++ code/new_gradmock_gen.py
@@ -58,6 +58,8 @@ def generate_gradmocks(grad_dim=grad_dim, path_to_lognorm_source=path_to_lognorm
 
     for mock_name in mock_info:
         # define mock
+        print(mock_name)
+        assert False
         mock = mock_info[mock_name]
         lognorm_file = str(mock[0])
         m = float(mock[1])

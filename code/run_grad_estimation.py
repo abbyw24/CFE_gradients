e5822a9679cc836461713a49fe162d166233f4c7
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-25 17:12:11 -0400

    Update run_grad_estimation.py

--- code/run_grad_estimation.py
+++ code/run_grad_estimation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 
+import read_lognormal
 from corrfunc_ls import xi
 
 import globals
@@ -14,6 +15,6 @@ rmax = globals.rmax
 nbins = globals.nbins
 nthreads = globals.nthreads
 
-mock_2x = np.load("/scratch/ksf293/mocks/lognormal/cat_L750_n2e-4_z057_patchy_As2x/cat_L750_n2e-4_z057_patchy_As2x_lognormal_rlz0.bin", allow_pickle=True)
+mock_2x = read_lognormal.read("/scratch/ksf293/mocks/lognormal/cat_L750_n2e-4_z057_patchy_As2x/cat_L750_n2e-4_z057_patchy_As2x_lognormal_rlz0.bin")
 print(type(mock_2x))
 print(mock_2x.shape)

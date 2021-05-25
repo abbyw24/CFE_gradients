ca50b86f674ffe5f7f0175d7efe2729ab1e5da99
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-25 17:13:38 -0400

    Update run_grad_estimation.py

--- code/run_grad_estimation.py
+++ code/run_grad_estimation.py
@@ -17,4 +17,4 @@ nthreads = globals.nthreads
 
 mock_2x = read_lognormal.read("/scratch/ksf293/mocks/lognormal/cat_L750_n2e-4_z057_patchy_As2x/cat_L750_n2e-4_z057_patchy_As2x_lognormal_rlz0.bin")
 print(type(mock_2x))
-print(mock_2x.shape)
+print(mock_2x)

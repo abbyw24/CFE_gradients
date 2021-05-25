7b32b48cb17fc74682d3559d6620d74c9779a141
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-25 02:04:19 -0400

    updating file names

---
+++ code/run_patches_vs_suave.py
@@ -0,0 +1,15 @@
+# generate gradient mocks based on the specified grad_type in globals
+import gradmock_gen
+
+# run patches
+import patchify_xi
+
+import patches_lstsq_fit
+
+# run suave
+import suave
+
+# compare performance of patches and suave
+import scatter
+
+import histogram

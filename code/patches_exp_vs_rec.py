a60982ea38d8fc0fb49982306353fb906cddbb74
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-27 16:45:47 -0400

    Update patches_exp_vs_rec.py

--- code/patches_exp_vs_rec.py
+++ code/patches_exp_vs_rec.py
@@ -53,11 +53,12 @@ def patches_exp_vs_rec(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, n_p
 
         # projection of recovered onto expected
         grad_exp_norm = np.linalg.norm(grad_expected)
-        print(grad_exp_norm)
-        print(grad_exp_norm**2)
-        assert False
+        print("grad_exp_norm = ", grad_exp_norm)
+        print("grad_exp_norm**2 = ", grad_exp_norm**2)
 
-        # proj_rec_onto_exp = (np.dot(grad_recovered,grad_expected)/grad_exp_norm**2)*grad_expected
+        proj_rec_onto_exp = (np.dot(grad_recovered,grad_expected)/grad_exp_norm**2)*grad_expected
+
+        assert False
 
         # residual
         residual = grad_recovered - grad_expected

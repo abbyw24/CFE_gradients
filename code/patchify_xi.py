e7e5d9d5990aaf13d381bb959cbe77eb9e8b43f4
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-27 22:29:19 -0400

    Update patchify_xi.py

--- code/patchify_xi.py
+++ code/patchify_xi.py
@@ -114,10 +114,11 @@ def xi_in_patches(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, mock_fil
         results_xi_full = xi_ls(mock_data, rand_set, periodic, nthreads, rmin, rmax, nbins)
         xi_full = np.array(results_xi_full[1])
         # save values used to calculate xi
-        patch_data["nthreads"] = nthreads
-        patch_data["rmin"] = rmin
-        patch_data["rmax"] = rmax
-        patch_data["nbins"] = nbins
+        patch_info["periodic"] = periodic
+        patch_info["nthreads"] = nthreads
+        patch_info["rmin"] = rmin
+        patch_info["rmax"] = rmax
+        patch_info["nbins"] = nbins
 
         # define r_avg (this is the same for all xi)
         r_avg = results_xi_full[0]

fdc5fbd6d2a3b14b884b46a686e2741534e72127
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-05-30 16:26:42 -0400

    changed globals

--- code/run_patches_vs_suave.py
+++ code/run_patches_vs_suave.py
@@ -11,7 +11,7 @@ patchify_xi.xi_in_patches()
 
 import patches_lstsq_fit
 patches_lstsq_fit.patches_lstsq_allbins()
-# patches_lstsq_fit.patches_lstsq_fit_1bin(r_bin=2)
+patches_lstsq_fit.patches_lstsq_fit_1bin(r_bin=2)
 
 # # import patches_exp_vs_rec
 # # patches_exp_vs_rec.patches_exp_vs_rec()

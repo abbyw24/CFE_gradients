14030f04f5919be7c4936e537922a3f5e5ede083
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-01 18:32:00 -0400

    Update run_1script.py

--- code/run_1script.py
+++ code/run_1script.py
@@ -1,15 +1,10 @@
-# # run patches
-# import patchify_xi
-# patchify_xi.xi_in_patches()
+# run patches
+import patchify_xi
+patchify_xi.xi_in_patches()
 
-# import patches_lstsq_fit
-# patches_lstsq_fit.patches_lstsq_allbins()
-# patches_lstsq_fit.patches_lstsq_fit_1bin(r_bin=2)
-
-# import patches_stats
-# n_patches_list = [8, 27]
-# lognormal_density = "2e-4"
-# patches_stats.histogram_patches(n_patches_list, lognormal_density=lognormal_density, nbins=20)
+import patches_lstsq_fit
+patches_lstsq_fit.patches_lstsq_allbins()
+patches_lstsq_fit.patches_lstsq_fit_1bin(r_bin=2)
 
 # compare performance of patches and suave
 import patches_vs_suave_stats
@@ -20,3 +15,8 @@ grads_rec_suave = grads["grads_rec_suave"]
 patches_vs_suave_stats.scatter_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave)
 patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=20)
 patches_vs_suave_stats.stats_patches_suave(grads_exp, grads_rec_patches, grads_rec_suave)
+
+import patches_stats
+n_patches_list = [8, 27]
+lognormal_density = "2e-4"
+patches_stats.histogram_patches(n_patches_list, lognormal_density=lognormal_density, nbins=20)

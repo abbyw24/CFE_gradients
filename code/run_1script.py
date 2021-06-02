9b69acf6ea15eb8b10913df5825050c75fc8f2e9
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-02 17:57:03 -0400

    tweaked plot parameters

--- code/run_1script.py
+++ code/run_1script.py
@@ -3,7 +3,16 @@
 # lognormal_density = "2e-4"
 # patches_stats.histogram_patches(n_patches_list, lognormal_density=lognormal_density, nbins=30)
 
-import density_stats
-densities_list = ["4e-4", "2e-4", "1e-4"]
-method = "CFE"
-density_stats.histogram_densities(densities_list, method, nbins=30)
+# import density_stats
+# densities_list = ["4e-4", "2e-4", "1e-4"]
+# method = "CFE"
+# density_stats.histogram_densities(densities_list, method, nbins=30)
+
+import patches_vs_suave_stats
+grads = patches_vs_suave_stats.extract_grads_patches_suave()
+grads_exp = grads["grads_exp"]
+grads_rec_patches = grads["grads_rec_patches"]
+grads_rec_suave = grads["grads_rec_suave"]
+patches_vs_suave_stats.scatter_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave)
+patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=20)
+patches_vs_suave_stats.stats_patches_suave(grads_exp, grads_rec_patches, grads_rec_suave)

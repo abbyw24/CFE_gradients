d8481b959b0e4905760e523872157184a266bd4b
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-02 14:07:36 -0400

    Update run_1script.py

--- code/run_1script.py
+++ code/run_1script.py
@@ -1,12 +1,5 @@
-# compare performance of patches and suave
-import patches_vs_suave_stats
-grads = patches_vs_suave_stats.extract_grads_patches_suave()
-grads_exp = grads["grads_exp"]
-grads_rec_patches = grads["grads_rec_patches"]
-grads_rec_suave = grads["grads_rec_suave"]
-patches_vs_suave_stats.scatter_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave)
-patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=30)
-patches_vs_suave_stats.stats_patches_suave(grads_exp, grads_rec_patches, grads_rec_suave)
+import gradmock_gen
+gradmock_gen.generate_gradmocks()
 
 # import patches_stats
 # n_patches_list = [27, 8]

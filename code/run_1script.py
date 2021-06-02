361deecf8a1b4b040bfd3ab2153e1a77b663f30e
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-01 21:41:41 -0400

    Update run_1script.py

--- code/run_1script.py
+++ code/run_1script.py
@@ -1,12 +1,12 @@
-# compare performance of patches and suave
-import patches_vs_suave_stats
-grads = patches_vs_suave_stats.extract_grads_patches_suave()
-grads_exp = grads["grads_exp"]
-grads_rec_patches = grads["grads_rec_patches"]
-grads_rec_suave = grads["grads_rec_suave"]
-patches_vs_suave_stats.scatter_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave)
-patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=20)
-patches_vs_suave_stats.stats_patches_suave(grads_exp, grads_rec_patches, grads_rec_suave)
+# # compare performance of patches and suave
+# import patches_vs_suave_stats
+# grads = patches_vs_suave_stats.extract_grads_patches_suave()
+# grads_exp = grads["grads_exp"]
+# grads_rec_patches = grads["grads_rec_patches"]
+# grads_rec_suave = grads["grads_rec_suave"]
+# patches_vs_suave_stats.scatter_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave)
+# patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=20)
+# patches_vs_suave_stats.stats_patches_suave(grads_exp, grads_rec_patches, grads_rec_suave)
 
 import patches_stats
 n_patches_list = [27, 8]

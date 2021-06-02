770d991768f46452eb6657af7c3747fd12c201f4
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-02 14:43:04 -0400

    Update run_1script.py

--- code/run_1script.py
+++ code/run_1script.py
@@ -5,3 +5,8 @@ gradmock_gen.generate_gradmocks()
 # n_patches_list = [27, 8]
 # lognormal_density = "2e-4"
 # patches_stats.histogram_patches(n_patches_list, lognormal_density=lognormal_density, nbins=30)
+
+import density_stats
+densities_list = ["4e-4", "2e-4", "1e-4"]
+method = "patches"
+density_stats.histogram_densities(densities_list, method, nbins=30)

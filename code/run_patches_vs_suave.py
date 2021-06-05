8adaa5b76c8d830c837d0c60cf7a1eec7383a9db
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-05 19:29:17 -0400

    Update run_patches_vs_suave.py

--- code/run_patches_vs_suave.py
+++ code/run_patches_vs_suave.py
@@ -1,13 +1,13 @@
-import globals
-globals.initialize_vals()
+# import globals
+# globals.initialize_vals()
 
-# generate gradient mocks based on the specified grad_type in globals
-import gradmock_gen
-gradmock_gen.generate_gradmocks()
+# # generate gradient mocks based on the specified grad_type in globals
+# import gradmock_gen
+# gradmock_gen.generate_gradmocks()
 
-# run patches
-import patchify_xi
-patchify_xi.xi_in_patches()
+# # run patches
+# import patchify_xi
+# patchify_xi.xi_in_patches()
 
 import patches_lstsq_fit
 patches_lstsq_fit.patches_lstsq_allbins()
@@ -16,9 +16,9 @@ patches_lstsq_fit.patches_lstsq_fit_1bin(r_bin=2)
 # import patches_exp_vs_rec
 # patches_exp_vs_rec.patches_exp_vs_rec()
 
-# run suave
-import suave
-suave.suave_exp_vs_rec()
+# # run suave
+# import suave
+# suave.suave_exp_vs_rec()
 
 # compare performance of patches and suave
 import patches_vs_suave_stats

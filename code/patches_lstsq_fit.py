93235f0ae9f8578ec25adbaa9dd0932788d9c39f
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-11 11:33:25 -0400

    Update patches_lstsq_fit.py

--- code/patches_lstsq_fit.py
+++ code/patches_lstsq_fit.py
@@ -43,9 +43,7 @@ def patches_lstsq_allbins(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, 
         patch_info = np.load(os.path.join(path_to_data_dir, f"patch_data/{lognormal_density}/{n_patches}patches/{mock_file_name_list[i]}.npy"), allow_pickle=True).item()
         patch_centers = patch_info["patch_centers"]
         print("patch_centers: ", patch_centers)
-        assert False
-        patch_centers -= L
-        #patch_centers -= L/2
+        patch_centers -= L/2
             # this centers the fiducial point in the box
         r_avg = patch_info["r_avg"]
         xi_patches = patch_info["xi_patches"]

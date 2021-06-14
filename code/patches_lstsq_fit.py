584ebd867c90cba70cb254970d6b44b764ab78bc
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-14 18:08:07 -0400

    Update patches_lstsq_fit.py

--- code/patches_lstsq_fit.py
+++ code/patches_lstsq_fit.py
@@ -37,12 +37,10 @@ def patches_lstsq_allbins(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, 
         mock_file_name = mock_info["mock_file_name"]
         mock_name = mock_info["mock_name"]
         L = mock_info["boxsize"]
-        print(L)
         grad_expected = mock_info["grad_expected"]
 
         patch_info = np.load(os.path.join(path_to_data_dir, f"patch_data/{lognormal_density}/{n_patches}patches/{mock_file_name_list[i]}.npy"), allow_pickle=True).item()
         patch_centers = patch_info["patch_centers"]
-        print("patch_centers: ", patch_centers)
         patch_centers -= L/2
             # this centers the fiducial point in the box
         r_avg = patch_info["r_avg"]
@@ -134,7 +132,6 @@ def patches_lstsq_allbins(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, 
         patch_centers += L/2
         # resave patch info dictionary
         np.save(os.path.join(path_to_data_dir, f"patch_data/{lognormal_density}/{n_patches}patches/{mock_file_name}"), patch_info, allow_pickle=True)
-        print("patch_centers: ", patch_centers)
 
         plt.legend()
         fig.savefig(os.path.join(path_to_data_dir, f"plots/patches/{lognormal_density}/{n_patches}patches/lst_sq_fit/allbins/{mock_file_name}.png"))

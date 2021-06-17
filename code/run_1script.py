d764cf6277bc2bb5b932cc144d380afaa4194909
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-17 00:01:17 -0400

    Update run_1script.py

--- code/run_1script.py
+++ code/run_1script.py
@@ -14,9 +14,9 @@ patches_lstsq_fit.patches_lstsq_fit_1bin(r_bin=2)
 # method = "CFE"
 # density_stats.histogram_densities(densities_list, method, nbins=30)
 
-patch_info = np.load("/scratch/aew492/research-summer2020_output/1D/patch_data/2e-4/8patches/cat_L500_n2e-4_z057_patchy_As2x_lognormal_rlz0_m--1.000-L_b-0.500.npy", allow_pickle=True).item()
-print(patch_info)
-print(patch_info["grad_recovered"])
+# patch_info = np.load("/scratch/aew492/research-summer2020_output/1D/patch_data/2e-4/8patches/cat_L500_n2e-4_z057_patchy_As2x_lognormal_rlz0_m--1.000-L_b-0.500.npy", allow_pickle=True).item()
+# print(patch_info)
+# print(patch_info["grad_recovered"])
 
 import patches_vs_suave_stats
 grads = patches_vs_suave_stats.extract_grads_patches_suave()

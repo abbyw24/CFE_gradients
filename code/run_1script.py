4529f945f472a9cdb8c7768a1c4e86ee16041f9a
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-24 19:22:22 -0400

    Update run_1script.py

--- code/run_1script.py
+++ code/run_1script.py
@@ -1,2 +1,6 @@
 import patches_vs_suave_stats
+grads = patches_vs_suave_stats.extract_grads_patches_suave()
+grads_exp = grads["grads_exp"]
+grads_rec_patches = grads["grads_rec_patches"]
+grads_rec_suave = grads["grads_rec_suave"]
 patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=30)

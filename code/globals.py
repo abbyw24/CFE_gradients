a9f648a716885ce8a18fba032e24df7dc556917a
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-28 13:55:17 -0400

    Update globals.py

--- code/globals.py
+++ code/globals.py
@@ -12,10 +12,10 @@ def initialize_vals():
     grad_dim = 1        # dimension of w_hat in gradient mock
 
     global boxsize
-    boxsize = 1500
+    boxsize = 500
 
     global lognormal_density
-    lognormal_density = "1e-6"
+    lognormal_density = "1e-4"
 
     global path_to_lognorm_source
     path_to_lognorm_source = f"/scratch/ksf293/mocks/lognormal/cat_L{boxsize}_n{lognormal_density}_z057_patchy_As2x"
@@ -24,10 +24,10 @@ def initialize_vals():
     path_to_data_dir = f"/scratch/aew492/research-summer2020_output/{grad_dim}D"
 
     global grad_type
-    grad_type = "1rlz_per_m"
+    grad_type = "1m"
 
     global n_mocks
-    n_mocks = 41
+    n_mocks = 401
 
     global mocks_info
 

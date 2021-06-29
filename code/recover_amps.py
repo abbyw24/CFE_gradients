b6d039440b2b1e5248a39c6cdb4b9e939f1a8fdb
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-06-29 16:45:17 -0400

    Update recover_amps.py

--- code/recover_amps.py
+++ code/recover_amps.py
@@ -4,12 +4,12 @@ import os
 def recover_amps(mock_file_name_list, num_den, n_patches, method="suave"):
     amps_rec = np.empty((len(mock_file_name_list), 4))
     abs_path = "/scratch/aew492/research-summer2020_output/1D/"
-    if method is "suave":
+    if method == "suave":
         for i in range(len(mock_file_name_list)):
             suave_info = np.load(os.path.join(abs_path, f"suave_data/1e-4/{mock_file_name_list[i]}.npy"), allow_pickle=True).item()
             amps = suave_info["amps"]
             amps_rec[i] = amps
-    elif method is "patches":
+    elif method == "patches":
         for i in range(len(mock_file_name_list)):
             patch_info = np.load(os.path.join(abs_path, f"patch_data/1e-4/8patches/{mock_file_name_list[i]}.npy"), allow_pickle=True).item()
             b_fit, m_fit = patch_info["b_fit"], patch_info["m_fit"].flatten()

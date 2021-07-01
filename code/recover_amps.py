9720e847bcd19a8d94c1f822671527538bc57074
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-07-01 03:10:28 -0400

    Update recover_amps.py

--- code/recover_amps.py
+++ code/recover_amps.py
@@ -12,7 +12,7 @@ def recover_amps(mock_file_name_list, num_den, n_patches=8, method="suave"):
     elif method == "patches":
         for i in range(len(mock_file_name_list)):
             patch_info = np.load(os.path.join(abs_path, f"patch_data/{num_den}/{n_patches}patches/{mock_file_name_list[i]}.npy"), allow_pickle=True).item()
-            b_fit, m_fit = patch_info["b_fit"], patch_info["m_fit"]
+            b_fit, m_fit = patch_info["b_fit"], patch_info["m_fit"].flatten()
             amps_rec[i,0], amps_rec[i,1:] = b_fit, m_fit
     else:
         print("")

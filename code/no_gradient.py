ecd35f1dee5aab83b7a35f096eda0cca049db5a7
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-07-02 00:38:37 -0400

    Update no_gradient.py

--- code/no_gradient.py
+++ code/no_gradient.py
@@ -7,34 +7,28 @@ from corrfunc_ls import xi_ls
 import globals
 globals.initialize_vals()
 
-# OUTLINE
-# loop:
-#   pull up mock from /scratch/ksf293/mocks/lognormal
-#   * NO gradient injection– use 'plain' clustered mock
-#   run standard Corrfunc (same process as calculating xi_full)
+def xi_lognormal(mock, rlz, mock_dir='/scratch/ksf293/mocks/lognormal', randmult=2, periodic=globals.periodic, nthreads=globals.nthreads,
+    rmin=globals.rmin, rmax=globals.rmax, nbins=globals.nbins):
 
-mock_dir = '/scratch/ksf293/mocks/lognormal'
-mock = 'cat_L750_n2e-4_z057_patchy'
+    path_to_mocks_dir = os.path.join(mock_dir, mock)
+    lognorm_file = str(mock)+'_lognormal_rlz'+str(rlz)+'.bin'
 
-path_to_mocks_dir = os.path.join(mock_dir, mock)
+    # read in mock
+    Lx, Ly, Lz, N, data = read_lognormal.read(os.path.join(path_to_mocks_dir, lognorm_file))
+    L = Lx      # boxsize
+    x, y, z, vx, vy, vz = data.T
+        # i believe (?) the raw data is centered at L/2 (0-L)
+    mock_data = np.array([x, y, z]).T
+    center_mock(mock_data, 0, L)
 
-# the following should eventually be in the loop
-lognorm_file = 'cat_L750_n2e-4_z057_patchy_lognormal_rlz0.bin'
-# read in mock
-Lx, Ly, Lz, N, data = read_lognormal.read(os.path.join(path_to_mocks_dir, lognorm_file))
-L = Lx      # boxsize
-x, y, z, vx, vy, vz = data.T
-    # i believe (?) the raw data is centered at L/2 (0-L)
-mock_data = np.array([x, y, z]).T
-print(mock_data.shape)
-center_mock(mock_data, 0, L)
+    # standard Corrfunc
+    # random set
+    nd = len(x)
+    nr = randmult*nd
+    rand_set = np.random.uniform(0, L, (nr,3))
+    # compute landy-szalay!
+    r_avg, results_xi = xi_ls(mock_data, rand_set, periodic, nthreads, rmin, rmax, nbins)
 
-# standard Corrfunc
-# random set
-nd = len(x)
-nr = 2*nd
-rand_set = np.random.uniform(0, L, (nr,3))
+    return r_avg, results_xi
 
-# compute landy-szalay!
-r_avg, results_xi = xi_ls(mock_data, rand_set, globals.periodic, globals.nthreads, globals.rmin, globals.rmax, globals.nbins)
-print(results_xi)
+xi_lognormal('cat_L750_n2e-4_z057_patchy', 0)

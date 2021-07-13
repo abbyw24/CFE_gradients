e7b994aabbfcb2b0641b43131c42176da9b21893
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-07-13 00:03:55 -0400

    Update run_patches_vs_suave.py

--- code/run_patches_vs_suave.py
+++ code/run_patches_vs_suave.py
@@ -1,23 +1,23 @@
-from datetime import datetime
-start = datetime.now()
+# from datetime import datetime
+# start = datetime.now()
 
-# compute correlation function on lognormal mocks (no gradient!)
-import xi_lognormal
+# # compute correlation function on lognormal mocks (no gradient!)
+# import xi_lognormal
 
-# generate gradient mocks based on the specified grad_type in globals
-import gradmock_gen
-gradmock_gen.generate_gradmocks()
+# # generate gradient mocks based on the specified grad_type in globals
+# import gradmock_gen
+# gradmock_gen.generate_gradmocks()
 
-# run patches
-import patchify_xi
-patchify_xi.xi_in_patches()
+# # run patches
+# import patchify_xi
+# patchify_xi.xi_in_patches()
 
-import new_patches_lstsq_fit
-new_patches_lstsq_fit.patches_lstsq_fit()
+# import new_patches_lstsq_fit
+# new_patches_lstsq_fit.patches_lstsq_fit()
 
-# run suave
-import suave
-suave.suave_exp_vs_rec()
+# # run suave
+# import suave
+# suave.suave_exp_vs_rec()
 
 # compare performance of patches and suave
 import patches_vs_suave_stats
@@ -29,9 +29,9 @@ patches_vs_suave_stats.scatter_patches_vs_suave(grads_exp, grads_rec_patches, gr
 patches_vs_suave_stats.histogram_patches_vs_suave(grads_exp, grads_rec_patches, grads_rec_suave, nbins=30)
 patches_vs_suave_stats.stats_patches_suave(grads_exp, grads_rec_patches, grads_rec_suave)
 
-runtime = datetime.now() - start
-days, seconds = runtime.days, runtime.seconds
-hours = days * 24 + seconds // 3600
-minutes = (seconds % 3600) // 60
-seconds = seconds % 60
-print(f"total run time = {hours}:{minutes}.{seconds}")
+# runtime = datetime.now() - start
+# days, seconds = runtime.days, runtime.seconds
+# hours = days * 24 + seconds // 3600
+# minutes = (seconds % 3600) // 60
+# seconds = seconds % 60
+# print(f"total run time = {hours}:{minutes}.{seconds}")

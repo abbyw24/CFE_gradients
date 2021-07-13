8c1fdc54a98e7892d231be9c2b12069d42e512c9
Abby Williams <64762610+abbyw24@users.noreply.github.com>
2021-07-13 13:11:13 -0400

    Update recover_amps.py

--- code/recover_amps.py
+++ code/recover_amps.py
@@ -1,9 +1,10 @@
 import numpy as np
 import os
+import globals
 
-def recover_amps(mock_file_name_list, num_den, n_patches=8, method="suave"):
+def recover_amps(mock_file_name_list, num_den, nbins=globals.nbins, n_patches=8, method="suave"):
     amps_rec = np.empty((len(mock_file_name_list), 4))
-    xi_patches = []
+    xi_full_patches = np.zeros((len(mock_file_name_list), nbins))
     abs_path = "/scratch/aew492/research-summer2020_output/1D/"
     if method == "suave":
         for i in range(len(mock_file_name_list)):
@@ -17,7 +18,7 @@ def recover_amps(mock_file_name_list, num_den, n_patches=8, method="suave"):
             b_fit, m_fit = patch_info["b_fit"], patch_info["m_fit"].flatten()
             amps_rec[i,0], amps_rec[i,1:] = b_fit, m_fit
             xi = patch_info["xi_full"]
-            xi_patches.append(xi)
-        return amps_rec, xi_patches
+            xi_full_patches[i] = xi
+        return amps_rec, xi_full_patches
     else:
         print("")

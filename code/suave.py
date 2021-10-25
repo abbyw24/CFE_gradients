2034eb2f98d283321aef4efcbb643cd83fdf74c6
Abby Williams <aew492@log-3.nyu.cluster>
2021-10-25 11:24:28 -0400

    updated suave_grad function for bao_iterative

--- code/suave.py
+++ code/suave.py
@@ -1,14 +1,9 @@
 import numpy as np
-import scipy
-from scipy.interpolate import BSpline
-from scipy.interpolate import _bspl
 import matplotlib
 import matplotlib.pyplot as plt
 import os
 
 import Corrfunc
-from Corrfunc.bases import bao_bases
-from Corrfunc.io import read_lognormal_catalog
 from Corrfunc.theory.DDsmu import DDsmu
 from Corrfunc.utils import compute_amps
 from Corrfunc.utils import evaluate_xi
@@ -17,15 +12,18 @@ from colossus.cosmology import cosmology
 
 from create_subdirs import create_subdirs
 import generate_mock_list
+from center_mock import center_mock
 import globals
 
 globals.initialize_vals()  # brings in all the default parameters
 
+cat_tag = globals.cat_tag
 grad_dim = globals.grad_dim
 boxsize = globals.boxsize
 lognormal_density = globals.lognormal_density
-path_to_data_dir = globals.path_to_data_dir
-grad_type = globals.grad_type
+data_dir = globals.data_dir
+grad_dir = globals.grad_dir
+mock_type = globals.mock_type
 
 randmult = globals.randmult
 periodic = globals.periodic
@@ -64,31 +62,29 @@ def cosmo_bases(rmin, rmax, projfn, cosmo_base=None, ncont=2000,
     bases[:,1] = np.array(bs)
 
     np.savetxt(projfn, bases)
-    ncomponents = bases.shape[1]-1
     return bases
 
 
 # define function to estimate gradient using suave
-def suave_exp_vs_rec(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, plots=False):
+def suave_grad(grad_dim=grad_dim, grad_dir=grad_dir, cosmo=False, plots=False):
 
     # make sure all inputs have the right form
     assert isinstance(grad_dim, int)
-    assert isinstance(path_to_data_dir, str)
+    assert isinstance(grad_dir, str)
 
     # create the needed subdirectories
-    tag = f'L{int(boxsize)}_n{lognormal_density}'
-    suave_dir = f'suave_data/{tag}'
-    plots_dir = f'plots/suave/{tag}/grad_recovered'
+    basis_type = 'bao_fixed' if cosmo else 'bao_iterative'
+    suave_dir = f'suave_data/{cat_tag}/{basis_type}'
+    plots_dir = f'plots/suave/{cat_tag}/{basis_type}/grad_recovered' if cosmo else f''
 
     sub_dirs = [
         suave_dir,
         plots_dir
     ]
-    create_subdirs(path_to_data_dir, sub_dirs)
+    create_subdirs(grad_dir, sub_dirs)
 
     # parameters for suave (not already imported from globals)
-    ntopbins = 10
-    r_edges = np.linspace(rmin, rmax, ntopbins+1) 
+    r_edges = np.linspace(rmin, rmax, nbins+1) 
     ncont = 2000
     r_fine = np.linspace(rmin, rmax, ncont)
 
@@ -97,31 +93,32 @@ def suave_exp_vs_rec(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, plots
 
     proj_type = 'gradient'
     weight_type = 'pair_product_gradient'
-    projfn = 'cosmo_basis.dat'
 
-    # set basis
-    bases = cosmo_bases(rmin, rmax, projfn, redshift=0.57, bias=2.0)
-    ncomponents = 4*(bases.shape[1]-1)
-    r = bases[:,0]
-    base_vals = bases[:,1]
+    # basis if cosmo (outside of loop because it's the same basis for all realizations)
+    if cosmo:
+        projfn = os.path.join(data_dir, f'bases/bao_fixed/cosmo_basis.dat')
+        basis = cosmo_bases(rmin, rmax, projfn, redshift=0.57, bias=2.0)
+        ncomponents = 4*(basis.shape[1]-1)
 
     for i in range(len(mock_file_name_list)):
-        # load in mock and patch info
-        mock_info = np.load(os.path.join(path_to_data_dir, f"mock_data/{tag}/{mock_file_name_list[i]}.npy"), allow_pickle=True).item()
-        mock_file_name = mock_info["mock_file_name"]
-        mock_name = mock_info["mock_name"]
-        L = mock_info["boxsize"]
-        m = mock_info["m"]
-        b = mock_info["b"]
-        mock_data = mock_info["grad_set"]
-        grad_expected = mock_info["grad_expected"]
 
-        # if there are negative values, shift by L/2, to 0 to L
-        if np.any(mock_data <= 0):
-            mock_data += L/2
-        else:
-            print("input mock data must be from -L/2 to L/2 (values shifted during xi_in_patches")
-            assert False
+        # load bases
+        if not cosmo:
+            projfn = os.path.join(data_dir, f'bases/bao_iterative/tables/final_bases/basis_gradient_{cat_tag}_trrnum_{randmult}x_rlz{i}.dat')
+            basis = np.loadtxt(projfn)
+            ncomponents = 4*(basis.shape[1]-1)
+
+
+        # load in mock and patch info
+        mock_info = np.load(os.path.join(grad_dir, f'mock_data/{cat_tag}/{mock_file_name_list[i]}.npy'), allow_pickle=True).item()
+        mock_file_name = mock_info['mock_file_name']
+        mock_name = mock_info['mock_name']
+        L = mock_info['boxsize']
+        mock_data = mock_info['grad_set']
+        grad_expected = mock_info['grad_expected']
+
+        # center data points between 0 and L
+        center_mock(mock_data, 0, L)
 
         # create suave dictionary
         suave_info = {}
@@ -177,7 +174,7 @@ def suave_exp_vs_rec(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, plots
         # b_guess = 0.5
         # m_recovered_perL = w_cont_norm*b_guess*L
         grad_recovered = w_cont 
-        suave_info["grad_recovered"] = grad_recovered
+        suave_info['grad_recovered'] = grad_recovered
 
         # print("recovered gradient (a/a_0) =", grad_recovered)
 
@@ -187,7 +184,7 @@ def suave_exp_vs_rec(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, plots
         # mean squared error just to see for now how close we are
         mean_sq_err = (1/len(grad_expected))*np.sum((grad_recovered - grad_expected)**2)
         # print(f"mean squared error = {mean_sq_err}")
-        suave_info["mean_sq_err"] = mean_sq_err
+        suave_info['mean_sq_err'] = mean_sq_err
 
         # non-loop-required plot parameters
         v_min = -L/2.
@@ -220,32 +217,86 @@ def suave_exp_vs_rec(grad_dim=grad_dim, path_to_data_dir=path_to_data_dir, plots
             
             sm = plt.cm.ScalarMappable(cmap=cmap, norm=vs_norm)
             cbar = plt.colorbar(sm)
-            cbar.set_label(r'$v \,\, (\mathbf{x} = v\hat{e}_\mathrm{gradient} + \mathbf{x}_\mathrm{pivot})$', rotation=270, labelpad=12)
+            cbar.set_label(r"$v \,\, (\mathbf{x} = v\hat{e}_\mathrm{gradient} + \mathbf{x}_\mathrm{pivot})$", rotation=270, labelpad=12)
             ax.axhline(0, color='grey', lw=0.5)
             ax.set_ylim((-0.01, 0.12))
-            ax.set_xlabel(r'Separation $r$ ($h^{-1}\,$Mpc)')
-            ax.set_ylabel(r'$\xi(r)$')
+            ax.set_xlabel(r"Separation $r$ ($h^{-1}\,$Mpc)")
+            ax.set_ylabel(r"$\xi(r)$")
 
-            if grad_type == "1mock":
+            if mock_type == '1mock':
                 ax.set_title("")
             else:
                 ax.set_title(f"Recovered Gradient, {mock_name}")
 
-            fig.savefig(os.path.join(path_to_data_dir, f'{plots_dir}/{mock_file_name}.png'))
+            fig.savefig(os.path.join(grad_dir, f'{plots_dir}/{mock_file_name}.png'))
 
             plt.cla()
-            plt.close("all")
+            plt.close('all')
 
         # save other plot parameters
-        suave_info["r_avg"] = r_avg
-        suave_info["amps"] = amps
-        suave_info["r_fine"] = r_fine
-        suave_info["xi_locs"] = xi_locs
-        suave_info["proj_type"] = proj_type
-        suave_info["projfn"] = projfn
-        suave_info["weight_type"] = weight_type
+        suave_info['r_avg'] = r_avg
+        suave_info['amps'] = amps
+        suave_info['r_fine'] = r_fine
+        suave_info['xi_locs'] = xi_locs
+        suave_info['proj_type'] = proj_type
+        suave_info['projfn'] = projfn
+        suave_info['weight_type'] = weight_type
 
         # save suave info dictionary
-        np.save(os.path.join(path_to_data_dir, f'{suave_dir}/{mock_file_name}'), suave_info, allow_pickle=True)
+        np.save(os.path.join(grad_dir, f'{suave_dir}/{mock_file_name}'), suave_info, allow_pickle=True)
 
         print(f"suave --> {mock_file_name}")
+
+# function to run suave with any basis; NOT for gradient though
+def suave(x, y, z, boxsize, projfn,
+            proj_type='generalr',
+            randmult = globals.randmult,
+            nthreads=globals.nthreads,
+            rmin = globals.rmin,
+            rmax = globals.rmax,
+            periodic = globals.periodic,
+            ncont = 1000,
+            nmubins = 1,
+            mumax = 1.0
+            ):
+
+    # data
+    data = np.array([x, y, z])
+    center_mock(data, 0, boxsize)
+    nd = len(x)
+
+    # random set
+    nr = randmult*nd
+    xs_rand = np.random.uniform(0, boxsize, (3, nr))
+    xr, yr, zr = xs_rand
+
+    # other parameters for suave
+    r_edges = np.linspace(rmin, rmax, nbins+1) 
+    r_fine = np.linspace(rmin, rmax, ncont)
+
+    # basis
+    basis = np.loadtxt(projfn)
+    ncomponents = basis.shape[1]-1
+
+    # run the pair counts
+    dd_res, dd_proj, _ = DDsmu(1, nthreads, r_edges, mumax, nmubins, x, y, z,
+                                    boxsize=boxsize, periodic=periodic, proj_type=proj_type,
+                                    ncomponents=ncomponents, projfn=projfn)
+    dr_res, dr_proj, _ = DDsmu(0, nthreads, r_edges, mumax, nmubins, x, y, z,
+                                    X2=xr, Y2=yr, Z2=zr,
+                                    boxsize=boxsize, periodic=periodic, proj_type=proj_type,
+                                    ncomponents=ncomponents, projfn=projfn)
+    rr_res, rr_proj, qq_proj = DDsmu(1, nthreads, r_edges, mumax, nmubins,
+                                            xr, yr, zr, boxsize=boxsize,
+                                            periodic=periodic, proj_type=proj_type,
+                                            ncomponents=ncomponents, projfn=projfn)
+    
+    amps = compute_amps(ncomponents, nd, nd, nr, nr, dd_proj, dr_proj, dr_proj, rr_proj, qq_proj)
+    xi_proj = evaluate_xi(amps, r_fine, proj_type, rbins=r_edges, projfn=projfn)
+
+    # results
+    results = np.empty((ncont, 2))
+    results[:,0] = r_fine
+    results[:,1] = xi_proj
+
+    return results

fdeb350a350d098a799e3bca50962954419ff68a
Abby Williams <aew492@log-3.nyu.cluster>
2022-03-21 17:43:50 -0400

    added list of catalog tags to globals

--- code/globals.py
+++ code/globals.py
@@ -18,10 +18,10 @@ def initialize_vals():
     grad_dir = os.path.join(data_dir, f'gradient/{grad_dim}D')
 
     global boxsize
-    boxsize = 1000
+    boxsize = 750
 
     global lognormal_density
-    lognormal_density = "2e-4"
+    lognormal_density = "1e-4"
 
     global As
     As = 2
@@ -33,7 +33,7 @@ def initialize_vals():
         cat_tag = f'L{boxsize}_n{lognormal_density}_z057_patchy_As{As}x'
 
     global mock_type
-    mock_type = '1m'     # choose from '1rlz', '1m', '1rlz_per_m', '1mock', or 'lognormal'
+    mock_type = 'lognormal'     # choose from '1rlz', '1m', '1rlz_per_m', '1mock', or 'lognormal'
 
     global mock_tag
     mock_tag = 'lognormal' if mock_type == 'lognormal' else 'gradient'
@@ -47,8 +47,8 @@ def initialize_vals():
     global rlz
     rlz = 0
 
-    global n_mocks
-    n_mocks = 401 
+    global nmocks
+    nmocks = 401 
 
     global mocks_info
 
@@ -73,5 +73,27 @@ def initialize_vals():
     # patch values
     global n_patches
     n_patches = 8          # should be n_sides**3
+
+    # list of cat_tags
+    global cat_tags
+    cat_tags = [
+        'L1000_n4e-4_z057_patchy_As2x',
+        'L1500_n1e-6_z057_patchy_As2x',
+        'L500_n1e-6_z057_patchy_As2x',
+        'L750_n1e-4_z057_patchy',
+        'L750_n2e-4_z057_patchy_As2x',
+        'L1000_n1e-4_z057_patchy_As2x',
+        'L1500_n1e-4_z057_patchy_As2x',
+        'L500_n1e-4_z057_patchy_As2x',
+        'L500_n2e-4_z057_patchy_As2x',
+        'L750_n1e-4_z057_patchy_As2x',
+        'L750_n4e-4_z057_patchy',
+        'L1000_n2e-4_z057_patchy_As2x',
+        'L1500_n1e-5_z057_patchy_As2x',
+        'L500_n1e-5_z057_patchy_As2x',
+        'L500_n4e-4_z057_patchy_As2x',
+        'L750_n2e-4_z057_patchy',
+        'L750_n4e-4_z057_patchy_As2x'
+    ]
 
 #initialize_vals()

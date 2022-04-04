fixed generation of mock suite from 1 rlz with a range of gradients

code/generate_mock_list.py
@@ -21,18 +21,20 @@ def generate_mock_list(
     if mock_type == "1rlz":
 
         m_arr = np.linspace(-1.0, 1.0, nmocks)
-        b_arr = b * np.ones([nmocks])
-        lognorm_file_list = [f'{cat_tag}_lognormal_rlz{rlz}']
+        b_arr = b * np.ones(nmocks)
+        lognorm_file_list = []
+        mock_param_list = []
 
         for i in range(nmocks):
-            mock_param_list[i] = "m-{:.3f}-L_b-{:.3f}".format(m_arr[i], b_arr[i])
-            mock_file_name_list[i] = "{}_rlz{}_{}".format(cat_tag, rlz, mock_param_list[i])
+            lognorm_file_list.append(f'{cat_tag}_lognormal_rlz{rlz}')
+            mock_param_list.append("m-{:.3f}-L_b-{:.3f}".format(m_arr[i], b_arr[i]))
+            mock_file_name_list.append("{}_rlz{}_{}".format(cat_tag, rlz, mock_param_list[i]))
 
 
     elif mock_type == "1m":
 
-        m_arr = m * np.ones([nmocks])
-        b_arr = b * np.ones([nmocks])
+        m_arr = m * np.ones(nmocks)
+        b_arr = b * np.ones(nmocks)
         lognorm_file_list = []
         mock_param_list = []
 
@@ -46,7 +48,7 @@ def generate_mock_list(
 
         assert nmocks == 1 or nmocks == 41 or nmocks == 401, "'nmocks' must be 1, 41, or 401"
         m_arr = np.linspace(-1.0, 1.0, nmocks)
-        b_arr = b * np.ones([nmocks])
+        b_arr = b * np.ones(nmocks)
         lognorm_file_list = []
         mock_param_list = []
 
@@ -58,8 +60,8 @@ def generate_mock_list(
     
     elif mock_type == "1mock":     # (i.e. plots for poster)
         assert nmocks == 1, "'nmocks' must be 1"
-        m_arr = m * np.ones([nmocks])
-        b_arr = b * np.ones([nmocks])
+        m_arr = m * np.ones(nmocks)
+        b_arr = b * np.ones(nmocks)
         lognorm_file_list = [f"{cat_tag}_lognormal_rlz{rlz}"]
         mock_param_list = ["m-{:.3f}-L_b-{:.3f}".format(m, b)]
         mock_file_name_list = ["{}_rlz{}_{}".format(cat_tag, rlz, mock_param_list[0])]
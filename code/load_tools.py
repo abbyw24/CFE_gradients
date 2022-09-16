14bc30c980e3c24ac8b866d0f0f39b0893728795
Abby Williams <aew492@log-3.nyu.cluster>
2022-09-16 10:45:01 -0400

    helper functions to load mock results

---
+++ code/load_tools.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+
+import globals
+globals.initialize_vals()
+
+
+def load_suave_amps(cat_tag, m, b=0.5, rlzs=401, basis='bao_fixed', grad_dir=globals.grad_dir):
+    """Return a (n,4) array of the gradient amplitudes recovered with Suave."""
+    amps = np.empty((rlzs,4))
+    for rlz in range(rlzs):
+        suave_dict = np.load(os.path.join(grad_dir, f'suave_data/{cat_tag}/{basis}/{cat_tag}_rlz{rlz}_m-{m:.3f}-L_b-{b:.3f}.npy'), allow_pickle=True).item()
+        amps[rlz] = suave_dict['amps']
+    return amps
+
+
+def load_patch_amps(cat_tag, m, b=0.5, rlzs=401, npatches=8, grad_dir=globals.grad_dir):
+    """Return a (n,4) array of the gradient amplitudes recovered with the standard patches approach."""
+    amps = np.empty((rlzs,4))
+    for rlz in range(rlzs):
+        patch_dict = np.load(os.path.join(grad_dir, f'patch_data/{cat_tag}/{npatches}patches/test_dir/{cat_tag}_rlz{rlz}_m-{m:.3f}-L_b-{b:.3f}.npy'), allow_pickle=True).item()
+        amps[rlz] = patch_dict['theta'].flatten()
+    return amps

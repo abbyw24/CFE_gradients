11b375f9691cb3604f129324605b33060464a2a9
Abby Williams <aew492@log-3.nyu.cluster>
2022-10-04 13:10:58 -0400

    updated function to generate random catalogs

--- code/random_cat.py
+++ code/random_cat.py
@@ -6,7 +6,9 @@ import Corrfunc
 import globals
 globals.initialize_vals()
 
-def main(boxsize=globals.boxsize, nbar_str=globals.lognormal_density, data_dir=globals.data_dir, nx=3):
+def main(boxsize=globals.boxsize, nbar_str=globals.lognormal_density, data_dir=globals.data_dir, nx=globals.randmult):
+
+    s = time.time()
 
     tag = '_L{}_n{}'.format(boxsize, nbar_str)
 
@@ -39,28 +41,24 @@ def main(boxsize=globals.boxsize, nbar_str=globals.lognormal_density, data_dir=g
         get_rr_terms(random, savepos=rr_fn)
     else:
         print("File already exists! Exiting.", rr_fn)
+    
+    print('time: {}'.format(time.time()-s))
 
 def generate_random(nbar, boxsize, nx, savepos=None):
-    print("Making random catalog")
-    s = time.time()
     nr = nx * float(nbar) * int(boxsize)**3
     random = np.random.uniform(0, boxsize, (int(nr),3))
-    print('time: {}'.format(time.time()-s))
-    print("N: {}".format(int(nr)))
     if savepos:
         np.savetxt(savepos, random)
     return random
 
 def get_rr_terms(rand_set, savepos=None, nthreads=globals.nthreads, rmin=globals.rmin, rmax=globals.rmax, nbins=globals.nbins, periodic=False):
     print("Calculating rr term")
-    s = time.time()
     assert len(rand_set.T) == 3, 'check input random shape'
     x_rand, y_rand, z_rand = rand_set.T
     r_edges = np.linspace(rmin, rmax, nbins+1)
     rr_res = Corrfunc.theory.DD(1, nthreads, r_edges, x_rand, y_rand, z_rand, periodic=periodic)
-    print('time: {}'.format(time.time()-s))
     if savepos:
         np.save(savepos, rr_res, allow_pickle=True)
 
-if __name__=='__main__':
-    main(boxsize=750, nbar_str='2e-5', nx=3)
+# if __name__=='__main__':
+#     main(boxsize=750, nbar_str='2e-5', nx=3)

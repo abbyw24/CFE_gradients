639435307635ea9290b64099f9508e692460cd73
Abby Williams <aew492@log-3.nyu.cluster>
2023-01-15 17:23:20 -0500

    tweaked formatting

--- code/run_mockset.py
+++ code/run_mockset.py
@@ -1,35 +1,37 @@
 import gradmock_gen
 import xi_funcs
+import bao_iterative
+import scipy_fit
 
-# L = 1500
-# m = 1
-
-# L = 1000
-# m = 0.667
-
-# L = 750
-# m = 0.5
-
-L = 500
-m = 0.333
+compute_baseline = True
+compute_cfe = False
+compute_patches = True
 
 ### baseline scripts ###    
+if compute_baseline:
+    # generate gradient mocks
+    gradmock_gen.generate_gradmocks()
 
-# generate gradient mocks
-gradmock_gen.generate_gradmocks(L=L, m=m)
+    # Landy-Szalay
+    xi_funcs.xi_ls_mocklist(overwrite=True)
 
-# Landy-Szalay
-xi_funcs.xi_ls_mocklist(L=L, m=m)
 
 ### CFE ###
+if compute_cfe:
+    # BAO iterative
+    bao_iterative.main()
 
-# CFE gradient estimation
-xi_funcs.grad_cfe_mocklist(L=L, m=m)
+    # CFE gradient estimation
+    xi_funcs.grad_cfe_mocklist()
 
-# CFE
-xi_funcs.xi_cfe_mocklist(L=L, m=m)
+    # CFE
+    xi_funcs.xi_cfe_mocklist()
 
 
 ### standard method ###
+if compute_patches:
+    # 4-parameter fit to get bases
+    scipy_fit.main()
 
-# ...
+    # patches gradient estimation
+    xi_funcs.grad_patches_mocklist()
